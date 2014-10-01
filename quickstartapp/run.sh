d8c19331d10c95a80cd60903af623e35492b0195
Ming Fang <mingfang@mac.com>
2014-10-01 16:37:01 -0400

    fixed silly typo

--- quickstartapp/run.sh
+++ quickstartapp/run.sh
@@ -2,13 +2,13 @@
 
 while ! nc -vz localhost 7070;do sleep 1; done
 
-set PIO_HOME=/PredictionIo
+set PIO_HOME=/PredictionIO
 
 cd /quickstartapp
 python import.py
-/PredictionIo/bin/pio instance io.prediction.engines.itemrank
+/PredictionIO/bin/pio instance io.prediction.engines.itemrank
 cd io.prediction.engines.itemrank
-/PredictionIo/bin/pio register
-/PredictionIo/bin/pio train
-/PredictionIo/bin/pio deploy
+/PredictionIO/bin/pio register
+/PredictionIO/bin/pio train
+/PredictionIO/bin/pio deploy
 
