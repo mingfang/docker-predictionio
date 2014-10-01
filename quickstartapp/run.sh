34fe535188927a7bfcd737a7ef7902633abfe3a3
Ming Fang <mingfang@mac.com>
2014-10-01 00:44:25 -0400

    fixed PIO_HOME variable

--- quickstartapp/run.sh
+++ quickstartapp/run.sh
@@ -4,9 +4,9 @@ set PIO_HOME=/PredictionIo
 
 cd /quickstartapp
 python import.py
-$PIO_HOME/bin/pio instance io.prediction.engines.itemrank
+${PIO_HOME}/bin/pio instance io.prediction.engines.itemrank
 cd io.prediction.engines.itemrank
-$PIO_HOME/bin/pio register
-$PIO_HOME/bin/pio train
-$PIO_HOME/bin/pio deploy
+${PIO_HOME}/bin/pio register
+${PIO_HOME}/bin/pio train
+${PIO_HOME}/bin/pio deploy
 
