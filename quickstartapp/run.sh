e92c39aa2869128bd17ba9763b6046bbe6f965db
Ming Fang <mingfang@mac.com>
2014-10-01 16:25:16 -0400

    try to get this to work for a user

--- quickstartapp/run.sh
+++ quickstartapp/run.sh
@@ -6,9 +6,9 @@ set PIO_HOME=/PredictionIo
 
 cd /quickstartapp
 python import.py
-${PIO_HOME}/bin/pio instance io.prediction.engines.itemrank
+/PredictionIo/bin/pio instance io.prediction.engines.itemrank
 cd io.prediction.engines.itemrank
-${PIO_HOME}/bin/pio register
-${PIO_HOME}/bin/pio train
-${PIO_HOME}/bin/pio deploy
+/PredictionIo/bin/pio register
+/PredictionIo/bin/pio train
+/PredictionIo/bin/pio deploy
 
